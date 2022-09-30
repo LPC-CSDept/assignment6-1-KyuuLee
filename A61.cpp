6792ac520208b6d9f178e48ad8eab78b0e268757
Kayw Lee <kwooler@gmail.com>
2022-09-29 19:15:07 -0700

    1

--- A61.cpp
+++ A61.cpp
@@ -1,45 +1,47 @@
+//	Lab 5-11 : Problem Definition
+//	Make a program that find the prime numbers in the given range.
+//		example) find the prime numbers in the range [1..100]
+//		2, 3, 5, 7, ... , 97
+
 #include <iostream>
 using namespace std;
 
-int inputValidate(int, int);
-void getListprime(int, int);
+int inputvalid(int b, int e);
+void printPrime(int begin, int end);
 
 int main()
 {
 	int begin, end;
-	int res;
+	int i, p;
 
 	do
 	{
-		cout << "Enter your two inputs\n";
+		cout << "Enter your range [from, to] \n";
 		cin >> begin >> end;
-	} while (inputValidate(begin, end));
-
-	getListprime(begin, end);
+	} while (inputvalid(begin, end));
+	// } while (begin >= end || begin < 2 || end < 0);
+	printPrime(begin, end);
 }
 
-int inputValidate(int begin, int end)
+int inputvalid(int b, int e)
 {
-	if ((begin > end) || (begin < 2) || (end < 0))
-	{
-		cout << "Invalid input\n";
+	if (b >= e || b < 2 || e < 0)
 		return 1;
-	}
 	else
 		return 0;
 }
-
-void getListprime(int begin, int end)
+void printPrime(int begin, int end)
 {
-	int i, j;
-	for (i = begin; i <= end; i++)
+	int p, i;
+	for (p = begin; p <= end; p++)
 	{
-		for (j = 2; j <= (i / 2); j++)
-			if (i % j == 0)
+		if (p == 0 || p == 1)
+			continue;
+		//* for(i=2; i<p; i++)
+		for (i = 2; i <= (p / 2); i++)
+			if (p % i == 0)
 				break;
-		if (j > (i / 2))
-			cout << i << "\t ";
+		if (i > (p / 2))
+			cout << " Prime number : " << p << endl;
 	}
-	cout << "\t";
 }
-// comments
